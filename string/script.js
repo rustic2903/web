b7c2e108f78feed55873b27e08840a1147fa8893
Ruslan <rusticospanov@gmail.com>
2025-11-29 20:27:39 +0500

    Deleting needles folder

--- string/script.js
+++
@@ -1,7 +0,0 @@
-let welcomeEl = document.getElementById("welcome-el")
-let names = "Ruslan"
-let greeting = "Hi, my name is "
-
-welcomeEl.innerText = greeting + names 
-
-welcomeEl.innerText  += "👋"
