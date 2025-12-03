b9e59095ad9c60061952d9378c67e027e3bb1af1
Ruslan <rusticospanov@gmail.com>
2025-12-03 21:55:26 +0500

    Just for training

---
+++ script.js
@@ -0,0 +1,8 @@
+let just_btn = document.getElementById("doit-btn")
+let search_bar = document.getElementById("search-input")
+
+function just_do(){
+    let output = "Cangaradulation you did it!"
+    search_bar.value = output
+    console.log(output)
+}
